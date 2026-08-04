ba7f0187af8d784034584d22317ab0a3978c3a51
Varol-Parlak <pvarolyilmaz@gmail.com>
2026-08-04 21:05:40 +0300

    added cuda

--- nn_torch.py
+++ nn_torch.py
@@ -4,12 +4,15 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 #download the dataset
 transform = transforms.ToTensor()
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 train_loader = DataLoader(dataset=train_dataset, batch_size=128, shuffle=True)
 test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False)
+
 class SimpleNN(nn.Module):
     def __init__(self, in_features, hidden_features, out_features):
         super().__init__()
@@ -25,7 +28,7 @@ class SimpleNN(nn.Module):
         x = self.dense2(x)
         return x
 
-model = SimpleNN(in_features=784, hidden_features=128, out_features=10)
+model = SimpleNN(in_features=784, hidden_features=128, out_features=10).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -40,6 +43,7 @@ for epoch in range(EPOCHS):
     epoch_acc = 0
 
     for x_batch, y_batch in train_loader:   
+        x_batch, y_batch = x_batch.to(device), y_batch.to(device)
         outputs = model(x_batch)
         loss = criterion(outputs, y_batch)
 
