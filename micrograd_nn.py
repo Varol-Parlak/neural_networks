817f21df4813a96dbb7c205e475dabb36604d0ef
Varol-Parlak <pvarolyilmaz@gmail.com>
2026-08-15 16:18:47 +0300

    layer of neurons

--- micrograd_nn.py
+++ micrograd_nn.py
@@ -23,4 +23,19 @@ class Neuron(Module):
         return self.weight + [self.bias]        
 
     def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'} Neuron({len(self.weight)})"
+        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.weight)})"
+
+class Layer(Module):
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
