8917c2c68a3929c74da8183354b5ba7f62d85dce
Varol-Parlak <pvarolyilmaz@gmail.com>
2026-08-19 22:15:52 +0300

    added context window

--- bigram_model.py
+++ bigram_model.py
@@ -7,23 +7,28 @@ chars = sorted(list(set("".join(words))))
 stoi = {s: i+1 for i, s in enumerate(chars)}
 stoi['.'] = 0 
 
-xs, ys = [], []
-for w in words[:1]:
-    chs = ["."]+ list(w) + ["."]
-    for ch1, ch2 in zip(chs, chs[1:]):
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        xs.append(ix1)
-        ys.append(ix2)
+block_size = 3
+X, Y = [], []
+for w in words[:5]:
+    context = [0] * block_size
+    for ch in w + '.':
+        ix = stoi[ch]
+        X.append(context)
+        Y.append(ix)
+        context = context[1:] + [ix]
 
-xs = torch.tensor(xs)
-ys = torch.tensor(ys)
-
-xenc = F.one_hot(xs, num_classes=27).float()
+X = torch.tensor(X)
+Y = torch.tensor(Y)
+num = X.nelement()
 g = torch.Generator().manual_seed(2147483647)
-W = torch.randn((27,27))
-logits = xenc @ W
-counts = logits.exp()
-probs = counts / counts.sum(1, keepdim=True)
-loss = -probs[torch.arange(5), ys].log().mean()
-print(loss)
+W = torch.randn((27,27), requires_grad=True)
+for epoch in range(20):
+    xenc = F.one_hot(X, num_classes=27).float()
+    logits = xenc @ W
+    counts = logits.exp()
+    probs = counts / counts.sum(1, keepdim=True)
+    loss = -probs[torch.arange(num), Y].log().mean() + 0.01*(W**2).mean()
+    print(loss.item())
+    W.grad = None
+    loss.backward()
+    W.data += -10 * W.grad
