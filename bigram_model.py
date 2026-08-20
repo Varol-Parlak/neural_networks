c2c5e17e532f04a2efe1a3bc98b4cc2cc22dcd8a
Varol-Parlak <pvarolyilmaz@gmail.com>
2026-08-21 00:29:42 +0300

    learning loop

--- bigram_model.py
+++ bigram_model.py
@@ -19,16 +19,38 @@ for w in words[:5]:
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
-num = X.nelement()
+
+# num = X.nelement()
+# g = torch.Generator().manual_seed(2147483647)
+# W = torch.randn((27,27), requires_grad=True)
+# for epoch in range(20):
+#     xenc = F.one_hot(X, num_classes=27).float()
+#     logits = xenc @ W
+#     counts = logits.exp()
+#     probs = counts / counts.sum(1, keepdim=True)
+#     loss = -probs[torch.arange(num), Y].log().mean() + 0.01*(W**2).mean()
+#     print(loss.item())
+#     W.grad = None
+#     loss.backward()
+#     W.data += -10 * W.grad
+
 g = torch.Generator().manual_seed(2147483647)
-W = torch.randn((27,27), requires_grad=True)
-for epoch in range(20):
-    xenc = F.one_hot(X, num_classes=27).float()
-    logits = xenc @ W
-    counts = logits.exp()
-    probs = counts / counts.sum(1, keepdim=True)
-    loss = -probs[torch.arange(num), Y].log().mean() + 0.01*(W**2).mean()
+C = torch.rand((27, 2), generator=g, requires_grad=True)
+W1 = torch.randn((6, 100), generator=g, requires_grad=True)
+b1 = torch.rand(100, generator=g, requires_grad=True)
+W2 = torch.randn((100, 27), generator=g, requires_grad=True)
+b2 = torch.randn(27, generator=g, requires_grad=True)
+parameters = [C, W1, b1, W2, b2]
+epoch, lr = 20, 0.1 
+
+for _ in range(epoch):
+    emb = C[X]
+    h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
+    logits = h @ W2 + b2
+    loss = F.cross_entropy(logits, Y)
     print(loss.item())
-    W.grad = None
+    for p in parameters:
+        p.grad = None 
     loss.backward()
-    W.data += -10 * W.grad
+    for p in parameters:
+        p.data += -lr * p.grad
